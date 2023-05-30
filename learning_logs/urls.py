9ffa8b23fc83dc2832da352c26b53e91664b8bbc
Bryan Umpierre Moreira <bryanumpierremoreira@google.com>
2023-05-29 21:59:02 -0300

    topic.html create
    - The code looks all right
    - capt 18 done

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,10 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Página inicial
     path('', views.index, name='index'),
-    
+
+    # Mostra todos os assuntos
+    path('topics/', views.topics, name='topics'),
+
+    # Pagina de detalhes para um unico assunto
+    path('topics/<int:topic_id>', views.topic, name='topic'),
 ]
